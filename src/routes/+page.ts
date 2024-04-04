af9ab480eba1c79efd103ca9150d9900dec01e9c
apexu06 <jakob.zelger@outlook.at>
2024-04-04 20:33:35 +0200

    vercel fix ?

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
   // Kids are cruel and I love minors - Michael Jackson
-  const intro = await import(`./${'introduction'}.md`);
+  const intro = await import(`../lib/${'introduction'}.md`);
   return {
     content: intro.default,
   };
